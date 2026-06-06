44089584e66b0e9e78600f6ce2e2f19a24ade7e6
Jackson Fleege <148395567+jfleege@users.noreply.github.com>
2026-06-06 17:04:22 -0400

    testing js

--- assets/js/transitions.js
+++ assets/js/transitions.js
@@ -2,14 +2,15 @@ document.addEventListener("DOMContentLoaded", () => {
   const links = document.querySelectorAll("a[href]");
 
   links.forEach((link) => {
+    const href = link.getAttribute("href");
     const url = new URL(link.href, window.location.href);
 
     const isSameSite = url.origin === window.location.origin;
-    const isAnchor = link.getAttribute("href").startsWith("#");
+    const isAnchor = href.startsWith("#");
     const opensNewTab = link.target === "_blank";
-    const isEmail = link.href.startsWith("mailto:");
+    const isMail = link.href.startsWith("mailto:");
 
-    if (!isSameSite || isAnchor || opensNewTab || isEmail) return;
+    if (!isSameSite || isAnchor || opensNewTab || isMail) return;
 
     link.addEventListener("click", (event) => {
       event.preventDefault();
